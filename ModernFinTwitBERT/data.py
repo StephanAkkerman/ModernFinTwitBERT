b1644a9946e875343a96ebef0990d4f06ab9dd85
Stephan Akkerman <stephan.akkerman@gmail.com>
2025-03-02 15:47:31 +0100

    Add code for data generation (3 splits)

---
+++ ModernFinTwitBERT/data.py
@@ -0,0 +1,298 @@
+# > Imports
+# Standard library
+import html
+import random
+import re
+from collections import Counter
+
+import numpy as np
+import pandas as pd
+
+# Third party
+from datasets import Dataset, concatenate_datasets, load_dataset
+
+
+def preprocess_tweet(tweet: str) -> str:
+    """
+    Preprocess a tweet by replacing URLs, @mentions, and cash tags with tokens.
+
+    Parameters
+    ----------
+    tweet : str
+        The tweet to preprocess.
+
+    Returns
+    -------
+    str
+        The preprocessed tweet.
+    """
+    # Unescape HTML characters
+    tweet = html.unescape(tweet)
+
+    # Replace URLs wiht URL token
+    tweet = re.sub(r"http\S+", "[URL]", tweet)
+
+    # Replace @mentions with @USER token
+    tweet = re.sub(r"@\S+", "@USER", tweet)
+
+    # Replace cash tags with [TICKER] token
+    # tweet = re.sub(r"\$[A-Z]{1,5}\b", "[TICKER]", tweet)
+
+    return tweet
+
+
+def split_dataset(
+    dataset: Dataset, val_size: float = 0.1, test_size: float = 0.1
+) -> tuple:
+    """Split the dataset into 3 parts: train, validation, and test.
+
+    Parameters
+    ----------
+    dataset : Dataset
+        The dataset to split.
+    val_size : float, optional
+        The size of the validation set, by default 0.1
+    test_size : float, optional
+        The size of the test set, by default 0.1
+
+    Returns
+    -------
+    tuple
+        Datsets for training, validation, and testing.
+    """
+    # First split: 80% train, 20% temp (val + test)
+    split_ds = dataset.train_test_split(test_size=val_size + test_size, seed=42)
+
+    # Second split on the temporary set to get validation and test (split temp into 50/50)
+    temp_ds = split_ds["test"]
+    split_size = val_size / (val_size + test_size)
+    split_temp = temp_ds.train_test_split(test_size=split_size, seed=42)
+
+    return split_ds["train"], split_temp["train"], split_temp["test"]
+
+
+def load_pretraining_data(val_size: float = 0.1) -> tuple:
+    """
+    Loads all the pretraining datasets from the data/pretrain/preprocessed folder.
+    Excluding the test dataset.
+
+    Returns
+    -------
+    pd.DataFrame
+        The complete pretraining dataset as a pandas DataFrame.
+    """
+    dataset = load_dataset(
+        "StephanAkkerman/crypto-stock-tweets",
+        split="train",
+        cache_dir="datasets",
+    )
+
+    dataframe = preprocess_dataset(dataset)
+    return split_dataset(dataframe, val_size=val_size)
+
+
+def load_finetuning_data(val_size: float = 0.1) -> tuple:
+    """
+    Loads and preprocesses the finetuning data and splits it into a training and validation set.
+
+    Parameters
+    ----------
+    val_size : float, optional
+        The size of the validation set, by default 0.1
+
+    Returns
+    -------
+    tuple
+        The training and validation datasets.
+    """
+
+    # https://huggingface.co/datasets/TimKoornstra/financial-tweets-sentiment
+    # 0: neutral, 1: bullish, 2: bearish
+    dataset = load_dataset(
+        "TimKoornstra/financial-tweets-sentiment",
+        split="train",
+        cache_dir="datasets",
+    )
+
+    # Rename columns
+    dataset = dataset.rename_column("tweet", "text")
+    dataset = dataset.rename_column("sentiment", "label")
+
+    dataframe = preprocess_dataset(dataset)
+    return split_dataset(dataframe, val_size=val_size)
+
+
+def simple_oversample(dataset: Dataset) -> pd.DataFrame:
+    from imblearn.over_sampling import RandomOverSampler
+
+    dataframe = dataset.to_pandas()
+
+    # Extract texts and labels
+    texts = dataframe["text"].tolist()
+    labels = dataframe["label"].tolist()
+
+    # Define RandomOverSampler
+    ros = RandomOverSampler(random_state=0)
+
+    # Resample indices and labels
+    resampled_indices, resampled_labels = ros.fit_resample(
+        np.array(list(range(len(texts)))).reshape(-1, 1), labels
+    )
+
+    # Extract resampled texts based on the resampled indices
+    resampled_texts = [texts[i[0]] for i in resampled_indices]
+
+    # Convert back to dataframe
+    dataframe = pd.DataFrame({"text": resampled_texts, "label": resampled_labels})
+
+    # To dataset
+    return Dataset.from_pandas(dataframe)
+
+
+def synonym_oversample(dataset: Dataset) -> pd.DataFrame:
+    import nlpaug.augmenter.word as naw
+
+    dataframe = dataset.to_pandas()
+
+    # Extract texts and labels
+    texts = [example["text"] for example in dataset]
+    labels = [example["label"] for example in dataset]
+
+    # Determine class frequencies and find the maximum frequency
+    class_counts = Counter(labels)
+    max_count = max(class_counts.values())
+
+    # Initialize nlpaug synonym augmenter
+    augmenter = naw.SynonymAug(aug_src="wordnet")
+
+    # Augment data for each class to match the count of the majority class
+    augmented_texts = []
+    augmented_labels = []
+    for label in class_counts.keys():
+        class_texts = [text for text, lbl in zip(texts, labels) if lbl == label]
+        augment_count = max_count - class_counts[label]
+
+        # Randomly choose and augment texts from the class until reaching the desired count
+        while augment_count > 0:
+            for text in random.choices(
+                class_texts, k=min(augment_count, len(class_texts))
+            ):
+                augmented_text = augmenter.augment(text)
+                augmented_texts.append(augmented_text[0])
+                augmented_labels.append(label)
+                augment_count -= 1
+
+    # Combine original and augmented data
+    resampled_texts = texts + augmented_texts
+    resampled_labels = labels + augmented_labels
+
+    # Convert back to dataframe
+    dataframe = pd.DataFrame({"text": resampled_texts, "label": resampled_labels})
+
+    # To dataset
+    return Dataset.from_pandas(dataframe)
+
+
+def preprocess_dataset(dataset: Dataset) -> pd.DataFrame:
+    # Convert to pandas
+    dataframe = dataset.to_pandas()
+
+    # Set labels to int
+    if "label" in dataframe.columns:
+        dataframe["label"] = dataframe["label"].astype(int)
+
+        # Drop all columns that are not text or label
+        dataframe = dataframe[["text", "label"]]
+    else:
+        dataframe = dataframe[["text"]]
+
+    # Preprocess tweets
+    dataframe["text"] = dataframe["text"].apply(preprocess_tweet)
+
+    # Drop duplicates
+    dataframe = dataframe.drop_duplicates(subset=["text"])
+
+    # Drop empty text tweets
+    dataframe = dataframe.dropna(subset=["text"])
+
+    # Drop 1 word tweets
+    dataframe = dataframe[dataframe["text"].apply(lambda x: len(x.split()) > 1)]
+
+    return dataframe
+
+
+def adjust_labels(dataset):
+    # Original labels: 0: negative, 1: neutral, 2: positive
+    # New labels: 0: neutral, 1: bullish, 2: bearish
+    label_mapping = {
+        0: 2,  # negative to bearish
+        1: 0,  # neutral to neutral
+        2: 1,  # positive to bullish
+    }
+    dataset["label"] = label_mapping[dataset["label"]]
+    return dataset
+
+
+def load_tweet_eval():
+    # 0: negative, 1: neutral, 2: positive
+    # https://huggingface.co/datasets/tweet_eval/viewer/sentiment
+    dataset = load_dataset(
+        "tweet_eval",
+        cache_dir="data/pre-finetune/",
+        name="sentiment",
+    )
+
+    # Concatenate the splits into a single dataset
+    dataset = concatenate_datasets(
+        [dataset["train"], dataset["test"], dataset["validation"]]
+    )
+
+    # Change labels to match other datasets
+    dataset = dataset.map(adjust_labels)
+
+    dataframe = preprocess_dataset(dataset)
+    return split_dataset(dataframe)
+
+
+def kfold_pretraining_data(k: int = 5) -> tuple:
+    """
+    Split the pretraining data into k folds.
+
+    Parameters
+    ----------
+    k : int, optional
+        The number of folds, by default 5
+
+    Returns
+    -------
+    tuple
+        The training and validation datasets.
+    """
+    from sklearn.model_selection import KFold
+
+    training_datasets = []
+    validation_datasets = []
+
+    df = load_pretraining_data(val_size=0)[0].to_pandas()
+
+    # Assuming 'dataset' is a list or array of your data
+    kf = KFold(n_splits=k, shuffle=True, random_state=42)
+
+    # This will yield indices for 5 splits
+    for train_index, val_index in kf.split(df):
+        training_datasets.append(Dataset.from_pandas(df.iloc[train_index]))
+        validation_datasets.append(Dataset.from_pandas(df.iloc[val_index]))
+
+    return training_datasets, validation_datasets
+
+
+def load_test_data() -> Dataset:
+    """
+    Loads the pretraining test dataset.
+
+    Returns
+    -------
+    Dataset
+        The test dataset.
+    """
+    return load_finetuning_data()[0]
--- ModernFinTwitBERT/eval.py
+++ ModernFinTwitBERT/eval.py
@@ -1,2 +1,165 @@
-# Evaluate on a dataset
-# Compare to other models
+import matplotlib.pyplot as plt
+import seaborn as sns
+import wandb
+from sklearn.metrics import accuracy_score, confusion_matrix, f1_score
+from sklearn.preprocessing import LabelEncoder
+from transformers import AutoTokenizer, BertForSequenceClassification, pipeline
+from transformers.pipelines.pt_utils import KeyDataset
+
+from datasets import load_dataset
+
+
+class Evaluate:
+    def __init__(self, use_baseline: bool = False, baseline_model: int = 1):
+        if not use_baseline:
+            labels = ["NEUTRAL", "BULLISH", "BEARISH"]
+            self.model = BertForSequenceClassification.from_pretrained(
+                "output/ModernFinTwitBERT",
+                num_labels=len(labels),
+                id2label={k: v for k, v in enumerate(labels)},
+                label2id={v: k for k, v in enumerate(labels)},
+                attn_implementation="flash_attention_2",
+                device_map="cuda",
+                torch_dtype="auto",
+                cache_dir="models",
+            )
+            self.model.config.problem_type = "single_label_classification"
+            self.tokenizer = AutoTokenizer.from_pretrained("output/ModernFinTwitBERT")
+        else:
+            if baseline_model == 0:
+                model_name = "StephanAkkerman/FinTwitBERT-Sentiment"
+            elif baseline_model == 1:
+                model_name = "ProsusAI/finbert"
+            elif baseline_model == 2:
+                model_name = "yiyanghkust/finbert-tone"
+
+                self.model = BertForSequenceClassification.from_pretrained(
+                    model_name,
+                    cache_dir="models",
+                    device_map="cuda",
+                )
+                self.tokenizer = AutoTokenizer.from_pretrained(
+                    model_name,
+                    cache_dir="models",
+                )
+
+        self.model.eval()
+        # https://huggingface.co/docs/transformers/main_classes/pipelines#transformers.pipeline
+        self.pipeline = pipeline(
+            "text-classification", model=self.model, tokenizer=self.tokenizer, device=0
+        )
+
+    def encode(self, data):
+        return self.tokenizer(
+            data["text"], truncation=True, padding="max_length", max_length=512
+        )
+
+    def load_test_data(self, tokenize: bool = True):
+        dataset = load_dataset(
+            "financial_phrasebank",
+            cache_dir="data/finetune/",
+            split="train",
+            name="sentences_50agree",
+        )
+
+        # Rename sentence to text
+        dataset = dataset.rename_column("sentence", "text")
+
+        if tokenize:
+            # Apply the tokenize function to the dataset
+            tokenized_dataset = dataset.map(self.encode, batched=True)
+
+            # Set the format for pytorch tensors
+            tokenized_dataset.set_format(
+                type="torch",
+                columns=["input_ids", "token_type_ids", "attention_mask", "label"],
+            )
+
+            return tokenized_dataset
+        return dataset
+
+    def evaluate_model(self):
+        true_labels, pred_labels = self.get_labels(category="finetune")
+        self.plot_confusion_matrix("eval", true_labels, pred_labels)
+
+        true_labels, pred_labels = self.get_labels(category="test")
+        self.calculate_metrics(true_labels, pred_labels)
+        self.plot_confusion_matrix("test", true_labels, pred_labels)
+
+    def get_labels(self, category: str, batch_size: int = 32):
+        if category == "test":
+            dataset = self.load_test_data(tokenize=False)
+            # Convert numerical labels to textual labels
+            true_labels = [
+                dataset.features["label"].int2str(label) for label in dataset["label"]
+            ]
+
+        elif category == "finetune":
+            _, dataset = load_finetuning_data()
+            # 0: neutral, 1: bullish, 2: bearish
+            int2str = {0: "neutral", 1: "bullish", 2: "bearish"}
+            true_labels = [int2str[label] for label in dataset["label"]]
+        else:
+            raise ValueError("Invalid category name")
+
+        pred_labels = []
+        for out in self.pipeline(KeyDataset(dataset, "text"), batch_size=batch_size):
+            pred_labels.append(out["label"].lower())
+
+        # Convert bullish to positive and bearish to negative
+        if category == "test":
+            label_mapping = {"bullish": "positive", "bearish": "negative"}
+            pred_labels = [label_mapping.get(label, label) for label in pred_labels]
+
+        return true_labels, pred_labels
+
+    def calculate_metrics(self, true_labels, pred_labels):
+        # Compute accuracy and F1 score
+        accuracy = accuracy_score(true_labels, pred_labels)
+        f1 = f1_score(true_labels, pred_labels, average="weighted")
+
+        # Log metrics to wandb
+        output = {
+            "test/final_accuracy": accuracy,
+            "test/final_f1_score": f1,
+        }
+
+        if wandb.run is not None:
+            wandb.log(output)
+
+        print(output)
+
+    def plot_confusion_matrix(self, category: str, true_labels, pred_labels):
+        # Create confusion matrix
+        label_encoder = LabelEncoder()
+        true_labels_encoded = label_encoder.fit_transform(true_labels)
+        pred_labels_encoded = label_encoder.transform(pred_labels)
+
+        cm = confusion_matrix(true_labels_encoded, pred_labels_encoded)
+        labels = label_encoder.classes_
+
+        # Plot confusion matrix
+        fig, ax = plt.subplots(figsize=(10, 8))
+        sns.heatmap(
+            cm,
+            annot=True,
+            fmt="d",
+            ax=ax,
+            cmap="Blues",
+            xticklabels=labels,
+            yticklabels=labels,
+        )
+        plt.ylabel("Actual")
+        plt.xlabel("Predicted")
+        plt.title("Confusion Matrix")
+
+        # Log confusion matrix to wandb
+        wandb.log({f"{category}/confusion_matrix": wandb.Image(fig)})
+
+        # Close the plot
+        plt.close(fig)
+
+
+if __name__ == "__main__":
+    eval = Evaluate(use_baseline=False)
+    eval.evaluate_model()
