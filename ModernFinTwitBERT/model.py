2cf7c31509f9a74d7cd994a56898019e5270457b
Stephan Akkerman <stephan.akkerman@gmail.com>
2025-03-13 21:41:14 +0100

    disable wandb during hyperopt runs

--- ModernFinTwitBERT/model.py
+++ ModernFinTwitBERT/model.py
@@ -33,20 +33,22 @@ class ModernFinTwitBERT:
         # Set the mode (finetune or pretrain)
         self.mode = self.config["mode"]
 
+        self.hyperopt = self.config[self.mode]["do_hyperopt"]
+
         # Get the mode-specific args
         self.mode_args = self.config[self.mode][f"{self.mode}_args"]
 
         self.model_name = self.config[self.mode]["model_name"]
 
-        labels = ["NEUTRAL", "BULLISH", "BEARISH"]
+        self.labels = ["NEUTRAL", "BULLISH", "BEARISH"]
 
         # Load the model
         if self.mode == "pretrain":
             self.model = ModernBertForMaskedLM.from_pretrained(
                 self.config[self.mode]["base_model"],
-                num_labels=len(labels),
-                id2label={k: v for k, v in enumerate(labels)},
-                label2id={v: k for k, v in enumerate(labels)},
+                num_labels=len(self.labels),
+                id2label={k: v for k, v in enumerate(self.labels)},
+                label2id={v: k for k, v in enumerate(self.labels)},
                 attn_implementation="flash_attention_2",
                 device_map="cuda",
                 torch_dtype="auto",
@@ -69,9 +71,9 @@ class ModernFinTwitBERT:
             # Need to update for our own pretrained model
             self.model = AutoModelForSequenceClassification.from_pretrained(
                 self.config[self.mode]["base_model"],
-                num_labels=len(labels),
-                id2label={k: v for k, v in enumerate(labels)},
-                label2id={v: k for k, v in enumerate(labels)},
+                num_labels=len(self.labels),
+                id2label={k: v for k, v in enumerate(self.labels)},
+                label2id={v: k for k, v in enumerate(self.labels)},
                 attn_implementation="flash_attention_2",
                 device_map="cuda",
                 torch_dtype="auto",
@@ -148,20 +150,84 @@ class ModernFinTwitBERT:
             compute_metrics if self.mode in ["finetune", "pre-finetune"] else None
         )
 
-        # Create a Trainer instance
-        trainer = Trainer(
-            model=self.model,
-            args=TrainingArguments(**self.mode_args, **self.config["base_args"]),
-            train_dataset=train,
-            eval_dataset=val,
-            data_collator=data_collator,
-            compute_metrics=compute_metrics_fn,
-        )
+        if self.hyperopt:
+            # https://huggingface.co/docs/transformers/en/hpo_train
+            # https://huggingface.co/docs/setfit/how_to/hyperparameter_optimization
+            # Create a Trainer instance
+            os.environ["WANDB_DISABLED"] = "true"
+            os.environ["WANDB_MODE"] = "disabled"
+
+            # Remove "report_to": "wandb" from args
+            self.config["base_args"].pop("report_to", None)
+
+            trainer = Trainer(
+                model=None,
+                args=TrainingArguments(**self.mode_args, **self.config["base_args"]),
+                train_dataset=train,
+                eval_dataset=val,
+                data_collator=data_collator,
+                compute_metrics=compute_metrics_fn,
+                model_init=self.model_init,
+            )
+
+            best_run = trainer.hyperparameter_search(
+                direction="maximize",
+                backend="optuna",
+                n_trials=10,
+                hp_space=optuna_hp_space,
+            )
+
+            # Apply the best hyperparameters and train one final time
+            trainer.apply_hyperparameters(best_run.hyperparameters, final_model=True)
+
+        else:
+            # Create a Trainer instance, without model_init
+            trainer = Trainer(
+                model=self.model,
+                args=TrainingArguments(**self.mode_args, **self.config["base_args"]),
+                train_dataset=train,
+                eval_dataset=val,
+                data_collator=data_collator,
+                compute_metrics=compute_metrics_fn,
+            )
+
+        print("Starting training")
+
         trainer.train()
+
+        print("Finished training")
 
         # Save the model
         trainer.save_model(self.output_dir)
         self.tokenizer.save_pretrained(self.output_dir)
+
+        print("Saved model and tokenizer")
+        return
+
+    def model_init(self, trial):
+
+        model = AutoModelForSequenceClassification.from_pretrained(
+            self.config[self.mode]["base_model"],
+            num_labels=len(self.labels),
+            id2label={k: v for k, v in enumerate(self.labels)},
+            label2id={v: k for k, v in enumerate(self.labels)},
+            attn_implementation="flash_attention_2",
+            device_map="cuda",
+            torch_dtype="auto",
+            cache_dir="models",
+        )
+        model.config.problem_type = "single_label_classification"
+        return model
+
+
+def optuna_hp_space(trial):
+
+    return {
+        "learning_rate": trial.suggest_float("learning_rate", 1e-6, 1e-4, log=True),
+        "per_device_train_batch_size": trial.suggest_categorical(
+            "per_device_train_batch_size", [16, 32, 64, 128]
+        ),
+    }
 
 
 if __name__ == "__main__":
